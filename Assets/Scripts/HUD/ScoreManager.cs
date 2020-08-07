ee9178d4d0b0d56fd36f41ece8e0b1801a03b959
Dan Pham <43536392+DaPhamius@users.noreply.github.com>
2020-08-07 03:10:43 +0200

    Reupload
    Reupload because of disruptive file

---
+++ Assets/Scripts/HUD/ScoreManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    public int score;
+    public Text scoreDisplay;
+
+    private void Update()
+    {
+        scoreDisplay.text = score.ToString();
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Obstacle") && score < 10)
+        {
+            score++;
+        }
+        else if (other.CompareTag("Obstacle") && score < 40)
+        {
+            score = score + 3;
+        }
+        else if (other.CompareTag("Obstacle") && score < 70)
+        {
+            score = score + 4;
+        }
+        else if (other.CompareTag("Obstacle") && score < 120)
+        {
+            score = score + 4;
+        }
+        else if (other.CompareTag("Obstacle") && score < 180)
+        {
+            score = score + 5;
+        }
+        else if (other.CompareTag("Obstacle") && score < 250)
+        {
+            score = score + 6;
+        }
+        else if (other.CompareTag("Obstacle") && score < 350)
+        {
+            score = score + 7;
+        }
+        else if (other.CompareTag("Obstacle") && score < 450)
+        {
+            score = score + 8;
+        }
+        else if (other.CompareTag("Obstacle") && score < 590)
+        {
+            score = score + 9;
+        }
+        else if (other.CompareTag("Obstacle") && score >= 590)
+        {
+            score = score + 10;
+        }
+
+                
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    public GameObject health1, health2, health3;
+    private Vector2 targetPos;
+    public float Yincrement;
+    public float speed;
+    public float maxHeight;
+    public float minHeight;
+
+    public int health;
+
+    public GameObject effect;
+
+    public GameObject gameOver;
+
+    public GameObject spawner;
+
+
+
+    // Update is called once per frame
+
+    private void Start()
+    {
+        health = 3;
+        health1.gameObject.SetActive(true);
+        health2.gameObject.SetActive(true);
+        health3.gameObject.SetActive(true);
+
+    }
+
+    void Update () {
+
+        if (health <= 0)
+        {
+            spawner.SetActive(false);
+            gameOver.SetActive(true);
+            Destroy(gameObject);
+        }
+
+        // health
+        switch (health)
+        {
+            case 3:
+                health1.gameObject.SetActive(true);
+                health2.gameObject.SetActive(true);
+                health3.gameObject.SetActive(true);
+                break;
+            case 2:
+                health1.gameObject.SetActive(true);
+                health2.gameObject.SetActive(true);
+                health3.gameObject.SetActive(false);
+                break;
+            case 1:
+                health1.gameObject.SetActive(true);
+                health2.gameObject.SetActive(false);
+                health3.gameObject.SetActive(false);
+                break;
+            case 0:
+                health1.gameObject.SetActive(false);
+                health2.gameObject.SetActive(false);
+                health3.gameObject.SetActive(false);
+                break;
+
+        }
+
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+		if (Input.GetKeyDown(KeyCode.W) && transform.position.y < maxHeight)
+        {
+            Instantiate(effect, transform.position, Quaternion.identity);
+
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+        }
+        else if (Input.GetKeyDown(KeyCode.S) && transform.position.y > minHeight)
+        {
+
+            Instantiate(effect, transform.position, Quaternion.identity);
+
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+
+        }
+    }
+}
